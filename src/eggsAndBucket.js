3763ac0acfe12727e134448f53966b3c9c1409db
Rustam_Samigullin <rstsmg1@gmail.com>
2018-11-04 17:40:14 -0500

    redone class Game and class Egg

--- src/eggsAndBucket.js
+++ src/eggsAndBucket.js
@@ -1,10 +1,13 @@
 class Player{
     constructor(){
-        this.name
-        this.score
-        this.lifes
-        this.location
+        this.name = name;
+        this.score = 0;
+        this.lifes = 5;
+        this.startingPoints = [1,2,3,4];
+        // location should one of the bucket divs 
+        this.location = 0;
     }
+
     canMove(){
 
     }
@@ -18,6 +21,69 @@ class Player{
 
     }
     right(){
-        
+
     }
+    startingPoint(){    
+        return this.startingPoints[(Math.floor(Math.random()*4))];
+   } 
 }
+
+class Egg{
+    constructor(position){
+        this.startingPoint = position;
+        
+        this.eggCoordinates1 = [[1,4], [3, 12]];
+        this.eggCoordinates2 = [[1,4], [3, 12]];
+        this.eggCoordinates3 = [[1,4], [3, 12]];
+        this.eggCoordinates4 = [[1,4], [3, 12]];
+        // this.position = {locX: 0, locY: 0};
+        // this.image = "../images/egg.jpg";
+        // startposition of egg should be at randomly selected egg's base.
+        // this.startPosition  = 0;
+
+    }
+    removeEggs(){
+
+    }
+    // depends of starting position 
+    move(){
+
+        if (this.startingPoint === 1 ){
+            console.log(1);
+            this.setEggTragectory(this.eggCoordinates1);
+            //$("#egg").addClass("div-top-left")
+        }   
+        if (this.startingPoint === 2 ){
+            console.log(2)
+            $("#egg").addClass("div-top-right")
+        }   
+        if (this.startingPoint === 3 ){
+            console.log(3)
+            $("#egg").addClass("div-bottom-right")
+        }   
+        if (this.startingPoint === 4){
+            console.log(4)
+            $("#egg").addClass("div-bottom-left")
+        }   
+     }
+
+     setEggTragectory(coordinates) {
+         //create egg div block at coordinate
+
+         let counter = 0;
+         //move egg along the coordinates 
+         let interval1 = setInterval(() => {
+            //draw egg at coordinates[counter];
+            counter ++;
+            if(counter === 4) {
+                //if player is in position 1
+                //add point to player 
+
+            }
+            //remove egg div from html
+            //close interval
+            interval1.clearInterval();
+         }, 2000);
+     }
+
+    }
--- src/game.js
+++ src/game.js
@@ -1,82 +1,185 @@
 class Game {
+    
     constructor(){
         this.playerTurn = 1;
-        this.startingPoints = [1,2,3,4];
-        this.startingPointforEggs = [1,2,3,4]
-        this.eggs = [1,2,3,4];
+       
+       
+        
+        //this.startingPointforEggs = [1,2,3,4]
+        // 
+        this.gamesIsOn = false;
+
+        
         this.spawnRate = 10;        
+        this.leftTop = document.getElementById("left-top");
+        this.leftBottom = document.getElementById("left-bottom")
+        this.rightTop = document.getElementById("right-top")
+        this.rightBottom = document.getElementById("right-bottom")
+        // buckets
+        this.leftTopBucket = document.getElementById("0.0");
+        this.leftBottomBucket = document.getElementById("0.1")
+        this.rightTopBucket = document.getElementById("1.0")
+        this.rightBottomBucket = document.getElementById("1.1")
+
+
+        this.timer = new Timer();
     }
-    // spawnEggs in the game
-    spawnEggs(egg){
-        this.eggs.push(egg)
-        if(this.eggs.length === 0 ){
-            this.eggs.push(egg)
-        }
+    
+   
+    spawnEggs(position){
+
+        let eggs = new Egg(position);
+        eggs.move();
     }
-    // removes egg from screen when it gets to end of line
+    
     removeEggs(element){
         return this.eggs.splice(indexOf(element),1);
     }
-    // get random starting point for bucket
-    startingPoint() 
-    {    
-         return this.startingPoints[(Math.floor(Math.random()*4))];
-    }   
-    // get starting point for egg base
+    
+    
     startingPointforBase(){
-        return this.startingPointforEggs[(Math.floor(Math.random()*4))]
+        return Math.floor(Math.random() * 4)+1;
     }
+    reset(){
+
+
+        this.leftTop.classList.remove("change-blue-to-white");
+        this.leftBottom.classList.remove("change-blue-to-white");
+        this.rightTop.classList.remove("change-blue-to-white");
+        this.rightBottom.classList.remove("change-blue-to-white");
+
+
+    }
+    resetForBuckets(){
+        this.leftTopBucket.classList.remove("change-blue-to-white");
+        this.leftBottomBucket.classList.remove("change-blue-to-white");
+        this.rightTopBucket.classList.remove("change-blue-to-white");
+        this.rightBottomBucket.classList.remove("change-blue-to-white");
+    }
+    resetGame(){
+            this.reset();
+            this.timer.reset();
+    }
+    startTimer(){
+
+        this.timer.start();
+       console.log(this.timer.getTimeValues())
+        this.timer.addEventListener('secondsUpdated', (e) => {
+            $('#values').html(this.timer.getTimeValues().toString());
+        })
+    }
+
 }
 
 
-let newGame = new Game();
-$(document).ready(function(){
+$(document).ready(function(){   
+    let newGame = new Game();
+    newGame.player = new Player();
+
     
 
-    $('#start-game-button').click(function () {
-        // Timer
-        let timer = new Timer();    
-        timer.start();
-        timer.addEventListener('secondsUpdated', function (e) {
-            $('#values').html(timer.getTimeValues().toString());
-        });
-        // Random position for bucket. Switch can get two buckets at the same time. Fixed by If Else statements
-        // console.log(newGame.startingPoint());
-
-
-        if (newGame.startingPoint()===1){
-            document.getElementById("0.0").classList.add("change-blue-to-white");
-        }
-        else if(newGame.startingPoint()===2){
-            document.getElementById("0.1").classList.add("change-blue-to-white");
-
-        }
-        else if(newGame.startingPoint()===3){
-            document.getElementById("1.0").classList.add("change-blue-to-white");
-    
-        }
-        else if(newGame.startingPoint()===4){
-            document.getElementById("1.1").classList.add("change-blue-to-white");
+    $('#start-game-button').click(function ()   {
+        // if (newGame.gamesIsOn)
+        // {
+        //     newGame.resetGame();   
+        // }
+        // newGame.gamesIsOn = true;
+        newGame.startTimer();
+      
         
-        }
-        else {
-            document.getElementById("1.0").classList.add("change-blue-to-white")
-        }
+      
+
         setInterval(function(){ 
-            newGame.startingPointforBase();
-            if(newGame.startingPointforBase()===1){
-            document.getElementById("left-top").classList.add("change-blue-to-white")
+            
+            let startingPointForBase = newGame.startingPointforBase();
+            if ((startingPointForBase===1)) {
+            newGame.spawnEggs(1);
+            newGame.reset();
+            document.getElementById("left-top").classList.add("change-blue-to-white");
             }
-            if(newGame.startingPointforBase()===2){
-                document.getElementById("left-bottom").classList.add("change-blue-to-white")
+            else if ((startingPointForBase===2)) {
+                newGame.spawnEggs(2);
+                newGame.reset();
+                document.getElementById("right-top").classList.add("change-blue-to-white");
             }
-            if(newGame.startingPointforBase()===3){
-                document.getElementById("right-top").classList.add("change-blue-to-white")
+            else if ((startingPointForBase===3)) {
+                newGame.spawnEggs(3);
+                newGame.reset();
+                document.getElementById("right-bottom").classList.add("change-blue-to-white");
             }
-            if(newGame.startingPointforBase()===4){
-                document.getElementById("right-bottom").classList.add("change-blue-to-white")
+            else if ((startingPointForBase===4)) {
+                newGame.spawnEggs(4);
+                newGame.reset();
+                document.getElementById("left-bottom").classList.add("change-blue-to-white");
             }
-        },1000)
+            // else 
+            // {
+            //     newGame.spawnEggs(4);
+            //     newGame.reset();
+            //     document.getElementById("right-top").classList.add("change-blue-to-white");
+            // }
+        }, 2000);
+       
+
+        
+        // else if(newGame.startingPointforBase()===2){
+        //     setInterval(function(){ 
+        //         newGame.reset()
+        //         newGame.spawnEggs(2);
+        //         document.getElementById("right-top").classList.add("change-blue-to-white");
+        //         },3000)
+            
+
+        // }
+        // else if(newGame.startingPointforBase()===3){
+           
+        //         newGame.reset()
+        //         newGame.spawnEggs(3);
+        //         document.getElementById("right-bottom").classList.add("change-blue-to-white");
+        //         },3000)
+    
+        // }
+        // else if(newGame.startingPointforBase()===4){
+        //     setInterval(function(){ 
+        //         newGame.reset()
+        //         newGame.spawnEggs(4);
+        //         document.getElementById("left-bottom").classList.add("change-blue-to-white");
+        //         },3000)
+        
+        // }
+        // else{
+        //     setInterval(function(){ 
+        //         newGame.reset()
+        //         newGame.spawnEggs(4);
+        //         document.getElementById("left-bottom").classList.add("change-blue-to-white");
+        //         },3000)
+        // }
+        
+        
+       
+
+
+
+        // setInterval(function(){ 
+        //     newGame.startingPointforBase();
+        //     if(newGame.startingPointforBase()===1){
+                
+        //         newGame.reset();
+        //     document.getElementById("left-top").classList.add("change-blue-to-white")
+        //     }
+        //     if(newGame.startingPointforBase()===2){
+        //         newGame.reset();
+        //         document.getElementById("left-bottom").classList.add("change-blue-to-white")
+        //     }
+        //     if(newGame.startingPointforBase()===3){
+        //         newGame.reset();
+        //         document.getElementById("right-top").classList.add("change-blue-to-white")
+        //     }
+        //     if(newGame.startingPointforBase()===4){
+        //         newGame.reset();
+        //         document.getElementById("right-bottom").classList.add("change-blue-to-white")
+        //     }
+        // },500)
         
 });
 
@@ -100,3 +203,4 @@ $(document).ready(function(){
         }
     
 }})
+
